1eaf9e6311964b545b80d60d87048a66479a4534
Dadihans23 <Dadihans06@gmail.com>
2025-11-03 00:25:11 +0100

    screens about subscription and statpage

--- treatment/admin.py
+++ treatment/admin.py
@@ -1,6 +1,10 @@
 
 # Register your models here.
 from django.contrib import admin
-from .models import CorrectionHistory
+from .models import CorrectionHistory , ChatMessage , ChatSession
 
 admin.site.register(CorrectionHistory)
+admin.site.register(ChatMessage)
+admin.site.register(ChatSession)
+
+
--- treatment/serializers.py
+++ treatment/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from .models import ExerciseProcessing
 from rest_framework import serializers
-from .models import CorrectionHistory
+from .models import CorrectionHistory , ChatMessage , ChatSession
+# backend/serializers.py
+from rest_framework import serializers
+from .models import ChatSession, ChatMessage
+
+
+
+
 
 class ProcessImageSerializer(serializers.Serializer):
     image = serializers.ImageField()
@@ -23,3 +30,31 @@ class CorrectionHistorySerializer(serializers.ModelSerializer):
     class Meta:
         model = CorrectionHistory
         fields = '__all__'        
+        
+
+
+
+class ChatMessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ChatMessage
+        fields = ['id', 'role', 'content', 'created_at', 'gemini_response_time']
+        read_only_fields = ['created_at', 'gemini_response_time']
+
+class ChatSessionListSerializer(serializers.ModelSerializer):
+    last_message = serializers.SerializerMethodField()
+    message_count = serializers.IntegerField(source='messages.count', read_only=True)
+
+    class Meta:
+        model = ChatSession
+        fields = ['id', 'title', 'created_at', 'updated_at', 'message_count', 'last_message']
+
+    def get_last_message(self, obj):
+        last = obj.messages.last()
+        return ChatMessageSerializer(last).data if last else None
+
+class ChatSessionDetailSerializer(serializers.ModelSerializer):
+    messages = ChatMessageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = ChatSession
+        fields = ['id', 'title', 'created_at', 'updated_at', 'messages']
