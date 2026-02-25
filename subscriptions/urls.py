4c9fe006df60614061aa47df9a9ed4c0c82de95e
Dadihans23 <Dadihans06@gmail.com>
2026-02-25 12:22:25 +0100

    25/02

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -6,11 +6,13 @@ app_name = 'subscriptions'
 
 
 urlpatterns = [
-    # ... tes autres URLs
     path('create-packs/', views.PackListCreateView.as_view(), name='pack-list-create'),
     path('subscribe/', views.SubscribeToPackView.as_view(), name='subscribe'),
     path('my-subscription/', views.MySubscriptionView.as_view(), name='my-subscription'),
     path('subscription-history/', views.subscription_history, name='subscription-history'),
     path('transactions/', views.TransactionListView.as_view(), name='transactions'),
     path('dashboard/', views.SubscriptionDashboardView.as_view(), name='subscription-dashboard'),
+    # === PAIEMENT MONEYFUSION ===
+    path('payment/webhook/', views.moneyfusion_webhook, name='payment-webhook'),
+    path('payment/status/<str:token_pay>/', views.check_payment_status, name='payment-status'),
 ]
